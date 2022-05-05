9b7f4ac6582e598f5c2b43317ddcbdcd80d05f6e
hackerrank <ea_ehd021279IT@yahoo.com>
2022-05-05 23:37:52 +0800

    updated

--- testjs/modules.js
+++ testjs/modules.js
@@ -1,1 +1,16 @@
-console.log(arguments)
+//console.log(arguments)
+
+
+//exports
+const C = require('./test-module-1')
+const c = new C();
+console.log(c.add(5,10));
+const calc2 = require('./test-module-2')
+console.log(calc2.add(2,6))
+const { add, multiply, divide } = require('./test-module-2')
+console.log(multiply(4,5))
+
+//caching
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();
